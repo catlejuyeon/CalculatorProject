5f093b8fe5551e61ecb3ed9223cf08e23f44302c
castlejuyawn <jucess1106@naver.com>
2025-10-14 16:54:32 +0900

    Lv.1 입력받은 양의 정수 2개와 사칙연산 기호를 사용하여 연산을 진행한 후 결과값을 출력하기(반복문 구현안함)

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -1,7 +1,39 @@
 package com.example.calculator;
 
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Hello Calculator");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("첫 번째 양의 정수 입력하세요: ");
+        int num1=scanner.nextInt();
+        System.out.println("두 번째 양의 정수 입력하세요: ");
+        int num2=scanner.nextInt();
+        System.out.println("사칙연산 기호를 입력하세요(+,-,*,/: ");
+        String operator = scanner.next();
+
+
+        switch (operator) {
+            case "+":
+                System.out.println("결과: " + (num1 + num2));
+                break;
+            case "-":
+                System.out.println("결과: " + (num1 - num2));
+                break;
+            case "*":
+                System.out.println("결과: " + (num1 * num2));
+                break;
+            case "/":
+                if (num2 == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                } else {
+                    double divResult = (double) num1 / num2;
+                    System.out.println("결과: " + divResult);
+                }
+                break;
+            default:
+                System.out.println("잘못된 연산 기호입니다.");
+        }
     }
 }
