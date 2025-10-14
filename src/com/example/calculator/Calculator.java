6a6583124d28acb0246c1d827f214bb6f1b9dbd1
castlejuyawn <jucess1106@naver.com>
2025-10-14 17:12:51 +0900

    Lv.1 클래스 없이 기본적인 연산을 수행할 수 있는 계산기 만들기(구현완료,반복문까지)

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -6,34 +6,42 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("첫 번째 양의 정수 입력하세요: ");
-        int num1=scanner.nextInt();
-        System.out.println("두 번째 양의 정수 입력하세요: ");
-        int num2=scanner.nextInt();
-        System.out.println("사칙연산 기호를 입력하세요(+,-,*,/: ");
-        String operator = scanner.next();
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1 = scanner.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = scanner.nextInt();
+            System.out.print("사칙연산 기호를 입력하세요 (+, -, *, /): ");
+            String operator = scanner.next();
 
+            switch (operator) {
+                case "+":
+                    System.out.println("결과: " + (num1 + num2));
+                    break;
+                case "-":
+                    System.out.println("결과: " + (num1 - num2));
+                    break;
+                case "*":
+                    System.out.println("결과: " + (num1 * num2));
+                    break;
+                case "/":
+                    if (num2 == 0) {
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    } else {
+                        double divResult = (double) num1 / num2;
+                        System.out.println("결과: " + divResult);
+                    }
+                    break;
+                default:
+                    System.out.println("잘못된 연산 기호입니다.");
+            }
 
-        switch (operator) {
-            case "+":
-                System.out.println("결과: " + (num1 + num2));
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String next = scanner.next();
+            if (next.equalsIgnoreCase("exit")) {
+                System.out.println("계산기를 종료합니다.");
                 break;
-            case "-":
-                System.out.println("결과: " + (num1 - num2));
-                break;
-            case "*":
-                System.out.println("결과: " + (num1 * num2));
-                break;
-            case "/":
-                if (num2 == 0) {
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                } else {
-                    double divResult = (double) num1 / num2;
-                    System.out.println("결과: " + divResult);
-                }
-                break;
-            default:
-                System.out.println("잘못된 연산 기호입니다.");
+            }
         }
     }
 }
